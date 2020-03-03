15d8d11d0a092f53f3638f3c52b6c315356fc1e3
Panagiotis Ressos <p.ressos@hotmail.com>
2020-03-03 19:33:46 +0200

    Task 4 - Added implementation for Create Book + fixed BookConverter + minor refactor

--- src/main/java/com/persado/assignment/project/controller/BookController.java
+++ src/main/java/com/persado/assignment/project/controller/BookController.java
@@ -1,32 +1,53 @@
 package com.persado.assignment.project.controller;
 
+import com.persado.assignment.project.model.dto.BookDto;
+import com.persado.assignment.project.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
 @Controller
 public class BookController {
 
-	@RequestMapping({"bookForm","/bookForm","/bookForm.html","book/bookForm","book/bookForm.html","/book/bookForm",
+	private final BookService bookService;
+
+	@RequestMapping({"bookForm", "/bookForm", "/bookForm.html", "book/bookForm", "book/bookForm.html", "/book/bookForm",
 			"/book/bookForm.html", "book/createForm", "/book/createForm", "book/createForm.hmtl", "/book/createForm.html"})
-	public String createBookPage() {
+	public String createBookPage(Model model) {
+
+		model.addAttribute("book", new BookDto());
 		return "book/bookForm";
 	}
 
-	@GetMapping({"books","/books","/books.html","books/index","books/index.html","/books/index","/books/index.html"})
+	@GetMapping(
+			{"books", "/books", "/books.html", "books/index", "books/index.html", "/books/index", "/books/index.html"})
 	public String showBooksPage() {
 		return "book/index";
 	}
 
-	@RequestMapping({"loan", "/loan", "loan.html", "/loan.html", "book/loan", "/book/loan", "book/loan.html", "/book/loan.html"})
+	@RequestMapping(
+			{"loan", "/loan", "loan.html", "/loan.html", "book/loan", "/book/loan", "book/loan.html", "/book/loan.html"})
 	public String loanBookPage() {
 		return "book/loan";
 	}
 
-	@RequestMapping({"returnBook", "/returnBook", "returnBook.html", "/returnBook.html", "book/return", "/book/return", "book/return.html", "/book/return.html"})
+	@RequestMapping(
+			{"returnBook", "/returnBook", "returnBook.html", "/returnBook.html", "book/return", "/book/return", "book/return.html", "/book/return.html"})
 	public String returnBookPage() {
 		return "book/return";
 	}
 
+	@PostMapping("book")
+	public String createBook(@Valid BookDto book) {
+		bookService.saveBook(book);
+
+		return "redirect:/books";
+	}
 
 }
--- src/main/java/com/persado/assignment/project/converter/BookConverter.java
+++ src/main/java/com/persado/assignment/project/converter/BookConverter.java
@@ -12,28 +12,36 @@ public class BookConverter implements Converter<Book, BookDto> {
 	private final UserConverter userConverter;
 
 	@Override
-	public Book toEntity(BookDto book) {
-		return Book.builder()
-				.id(book.getId())
-				.name(book.getName())
-				.summary(book.getSummary())
-				.isbn(book.getIsbn())
-				.purchased(book.getPurchased())
-				.available(book.getAvailable())
-				.users(userConverter.toEntitiesList(book.getUsers()))
+	public Book toEntity(BookDto bookDto) {
+		Book book = Book.builder()
+				.id(bookDto.getId())
+				.name(bookDto.getName())
+				.summary(bookDto.getSummary())
+				.isbn(bookDto.getIsbn())
+				.purchased(bookDto.getPurchased())
+				.available(bookDto.getAvailable())
 				.build();
+		if (bookDto.getUsers() != null) {
+			book.setUsers(userConverter.toEntitiesList(bookDto.getUsers()));
+		}
+
+		return book;
 	}
 
 	@Override
 	public BookDto toDto(Book book) {
-		return BookDto.builder()
+		BookDto bookDto = BookDto.builder()
 				.id(book.getId())
 				.name(book.getName())
 				.summary(book.getSummary())
 				.isbn(book.getIsbn())
 				.purchased(book.getPurchased())
 				.available(book.getAvailable())
-				.users(userConverter.toDtosList(book.getUsers()))
 				.build();
+		if (book.getUsers() != null) {
+			bookDto.setUsers(userConverter.toDtosList(book.getUsers()));
+		}
+
+		return bookDto;
 	}
 }
